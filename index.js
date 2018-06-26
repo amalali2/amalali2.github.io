9569eeb0b5f0f7d18fb59129ca14ed876bd49fc4
Amal Ali <amalali@Amals-MacBook-Pro.local>
2018-06-26 06:48:30 -0400

    added hamburger menu

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-$(window).on("load",function() {
-  $(window).scroll(function() {
-    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-    $(".fade").each(function() {
-      /* Check the location of each desired element */
-      var objectBottom = $(this).offset().top + $(this).outerHeight();
+$(document).on("scroll", function () {
 
-      /* If the element is completely within bounds of the window, fade it in */
-      if (objectBottom < windowBottom) { //object comes into view (scrolling down)
-        if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
-      } else { //object goes out of view (scrolling up)
-        if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
-      }
-    });
-  }).scroll(); //invoke scroll-handler on page-load
-});
+  var scrollTop = $(document).scrollTop()
+
+  var scrollBottom = scrollTop + $(window).height()
+
+  var pageBottom = $(document).height()
+
+var diff = pageButton - scrollBottom
+
+var opacity = 1 - diff / 300
+
+$("div.fade-bg").css("opacity", opacity)
+
+})
