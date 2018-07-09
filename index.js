335ba3341e8db533f0fda499ca7ce6531e1a478b
Amal Ali <amalali@Amals-MacBook-Pro.local>
2018-07-09 11:51:33 -0400

    second iteration

--- index.js
+++ index.js
@@ -1,15 +1,53 @@
-$(document).on("scroll", function () {
+var slideIndex = 1;
+showDivs(slideIndex);
 
-  var scrollTop = $(document).scrollTop()
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
 
-  var scrollBottom = scrollTop + $(window).height()
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
 
-  var pageBottom = $(document).height()
+var modal = document.getElementById('myModal');
 
-var diff = pageButton - scrollBottom
+var btn = document.getElementById("myBtn");
 
-var opacity = 1 - diff / 300
 
-$("div.fade-bg").css("opacity", opacity)
+var span = document.getElementsByClassName("close")[0];
 
-});
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+      }
+  window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+  var dropdowns = document.getElementsByClassName("dropdown-content");
+  var i;
+  for (i = 0; i < dropdowns.length; i++) {
+  var openDropdown = dropdowns[i];
+  if (openDropdown.classList.contains('show')) {
+  openDropdown.classList.remove('show');
+                          }
+                        }
+                      }
+                    }
