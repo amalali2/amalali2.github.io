1a78785c3485afd1a87b3214a192ad4490765c2f
Amal Ali <amalali@Amals-MacBook-Pro.local>
2018-07-23 15:15:27 -0400

    added hover animation

---
+++ js/projects.js
@@ -0,0 +1,23 @@
+// everytime we hover a link, run a function
+
+//inside of our function, we want to find out which overylay we want to show
+
+// remove .visible from all of the overlays
+
+//we add the .visible class onto 
+
+$('.project-box a').hover(function() {
+
+
+    // this refers to the current link we've just hovered
+    var link = $(this)
+
+    // here we grab the data-overlay attribute (which refers to the class we want to show)
+    var overlay = link.data('overlay')
+
+    // first we hide all of the overlay by removing the 'visible class
+    $('.overlay.bg').removeClass('visible')
+
+    // lastly we add the visible class 
+    $(overlay).addClass('visible')
+});
