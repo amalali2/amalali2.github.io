21086e20d00ad2080b91c470705bf2d75b0366ce
Amal Ali <amalali@Amals-MacBook-Pro.local>
2018-07-26 18:13:29 -0400

    added hamburger menu

--- js/index.js
+++ js/index.js
@@ -14,3 +14,9 @@ function myFunction() {
                         }
                       }
                     }
+
+                    $(".menu-toggle").on('click', function() {
+                      $(this).toggleClass("on");
+                      $('.menu-section').toggleClass("on");
+                      $("nav ul").toggleClass('hidden');
+                    });
