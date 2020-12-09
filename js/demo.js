f33df0a9b53ab84e0bb371aedb3688d45baee033
Amal <435amal@gmail.com>
2020-12-08 21:26:19 -0500

    update demo.js

--- js/demo.js
+++ js/demo.js
@@ -245,12 +245,25 @@
     /********** Preload stuff **********/
 
     // Preload images
-    const preloadImages = () => {
-       
-    };
+    function ready(callbackFunc) {
+        if (document.readyState !== 'loading') {
+          // Document is already ready, call the callback directly
+          callbackFunc();
+        } else if (document.addEventListener) {
+          // All modern browsers to register DOMContentLoaded
+          document.addEventListener('DOMContentLoaded', callbackFunc);
+        } else {
+          // Old IE browsers
+          document.attachEvent('onreadystatechange', function() {
+            if (document.readyState === 'complete') {
+              callbackFunc();
+            }
+          });
+        }
+      }
     
     // And then..
-    preloadImages().then(() => {
+    ready(function() {
         // Remove the loader
         document.body.classList.remove('loading');
         // Get the scroll position and update the lastScroll variable
