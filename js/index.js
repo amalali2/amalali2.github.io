0eb8156b8343d569ff612dbe92777daa1ec2ce08
Amal <435amal@gmail.com>
2024-03-30 01:11:21 -0400

    Site Refresh 1.0

--- js/index.js
+++ js/index.js
@@ -1,124 +1,51 @@
-// modal functions
+const headerTag = document.querySelector("header")
+const arrowTag = document.querySelector("img.arrow")
 
-  $(document).ready(function(){
-   
-    $(".img-wrap").on("click", function() {
-        if ($(this).attr("id")) {
-        $("body").removeClass('modal-is-closed');
-        $("body").addClass('modal-is-showing');
-        $('.modal').css('opacity','1');
-        $(".modal").show();
+const sections = document.querySelectorAll("section")
+const wavesBoxes = document.querySelectorAll("g.wave")
 
-
-        var itemId = $(this).attr("id");
-        var showItem = $('.item').attr("id")==itemId
-        
-        $('.project-content').find('[id="'+itemId+'"]').addClass('show');
-        }
-        
-    });
-    
-    $(".back-btn").on("click", function() {
-        $(".modal").animate({ scrollTop: $('.modal').prop("scrollHeight")}, 1000);
-    });
-
-    var elem = $('.modal');
-
-    elem.scroll(function(e) {
-        if (elem[0].scrollHeight - elem.scrollTop() <= elem.outerHeight()) {
-            $('.modal').css('opacity','0');
-            $('.modal').scrollTop(0);
-            $(".modal").hide();
-            $('.item').removeClass('show');
-        }
-    });
-
-    function hideModal() {     
-        $(".modal").delay(1000).hide();
-    }
-
-});
-
-// loading
-
-$(document).ready(function() {
-        
-    var counter = 0;
-    var c = 0;
-    var i = setInterval(function() {
-      
-      $(".loading-page .counter h1").html(c + '%');
-  
-      
-      counter++;
-      c++;
-      if(counter == 101) {
-        clearInterval(i);
-        $(".loading-page").animate({opacity: 0}, 1000);
-        setTimeout(function(){ 
-            $(".loading-page").hide();
-        }, 1001);
-      }
-    }, 50);
-  });
-
-
-// img reveal
-
-setTimeout(function(){ 
-anime({
-  targets: '.outer',
-  translateY: '-100%',
-  direction: 'reverse',
-  duration: 3000,
-  easing: 'easeInOutCubic',
-})
-
-anime({
-  targets: '.inner',
-  translateY: '100%',
-  direction: 'reverse',
-  duration: 3000,
-  easing: 'easeInOutCubic',
-})
-
-anime({
-  targets: '.image',
-  scale: 2,
-  easing: 'easeInOutQuad',
-  direction: 'reverse',
-  duration: 3500,
-})
-}, 5000);
-
-// scroll to bottom
-
-$('nav.name').click(function(){
-
-  var WH = $(window).height();  
-  var SH = $('body').prop("scrollHeight");
-  $('html, body').stop().animate({scrollTop: SH-WH}, 1000);
-
-}); 
-
-
-
-/**const body = document.body,
-      jsScroll = document.getElementsByClassName('js-scroll')[0],
-      height = jsScroll.getBoundingClientRect().height - 1,
-      speed = 0.05
-
-var offset = 0
-
-body.style.height = Math.floor(height) + "px"
-
-function smoothScroll() {
-    offset += (window.pageYOffset - offset) * speed
-    
-    var scroll = "translateY(-" + offset + "px) translateZ(0)"
-    jsScroll.style.transform = scroll
-    
-    raf = requestAnimationFrame(smoothScroll)
+const easing = function (x) {
+  return x * x * x;
 }
-smoothScroll()**/
+
+const fadeHeader = function () {
+  const pixels = window.pageYOffset
+  
+ 	headerTag.style.opacity = 1 - easing((pixels / 500))
+  
+ 	arrowTag.style.opacity = 1 - easing((pixels / 250))
+}
+
+const checkCircles = function () {  
+  sections.forEach((section, index) => {
+   	const waves = wavesBoxes[index].querySelectorAll("circle, path")
+    
+    if (section.getBoundingClientRect().top <= window.innerHeight / 2) {
+      waves.forEach(wave => {
+        wave.classList.add("in-view")
+      })
+    } else {
+      waves.forEach(wave => {
+        wave.classList.remove("in-view")
+      })	  	
+    }
+  })
+}
+
+sections.forEach(section => {
+  section.style.backgroundColor = "transparent"
+  section.style.color = "#000000"
+})
+
+checkCircles()
+fadeHeader()
+
+window.addEventListener("scroll", function () {
+  checkCircles()
+  fadeHeader()
+})
+
+
+
+
 
